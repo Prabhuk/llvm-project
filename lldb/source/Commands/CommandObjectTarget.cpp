875e1c0fb273d0f66d7764b26726e3f829b4578f
prabhukr <prabhukr@google.com>
2025-06-05 01:00:11 +0000

    Revert "Make stop-hooks fire when lldb first gains control of a process. (#137410)"
    This reverts commit 4fdb8cb42f73ebec9a3bdd37b2f27c222f9afd87.

--- lldb/source/Commands/CommandObjectTarget.cpp
+++ lldb/source/Commands/CommandObjectTarget.cpp
@@ -4806,17 +4806,6 @@ public:
         m_one_liner.push_back(std::string(option_arg));
         break;
 
-      case 'I': {
-        bool value, success;
-        value = OptionArgParser::ToBoolean(option_arg, false, &success);
-        if (success)
-          m_at_initial_stop = value;
-        else
-          error = Status::FromErrorStringWithFormat(
-              "invalid boolean value '%s' passed for -F option",
-              option_arg.str().c_str());
-      } break;
-
       default:
         llvm_unreachable("Unimplemented option");
       }
@@ -4843,7 +4832,6 @@ public:
       m_use_one_liner = false;
       m_one_liner.clear();
       m_auto_continue = false;
-      m_at_initial_stop = true;
     }
 
     std::string m_class_name;
@@ -4864,7 +4852,6 @@ public:
     // Instance variables to hold the values for one_liner options.
     bool m_use_one_liner = false;
     std::vector<std::string> m_one_liner;
-    bool m_at_initial_stop;
 
     bool m_auto_continue = false;
   };
@@ -5029,9 +5016,6 @@ protected:
 
     if (specifier_up)
       new_hook_sp->SetSpecifier(specifier_up.release());
-
-    // Should we run at the initial stop:
-    new_hook_sp->SetRunAtInitialStop(m_options.m_at_initial_stop);
 
     // Next see if any of the thread options have been entered:
 
